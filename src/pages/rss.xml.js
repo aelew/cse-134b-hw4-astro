64d8c544ba13b369b37e9c3c72d0e656f7584c0c
Andre Lew <me@aelew.com>
2025-11-17 18:29:18 -0800

    feat: refactor + add recipes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,14 +4,14 @@ import { getCollection } from 'astro:content';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
-  const posts = await getCollection('blog');
+  const recipes = await getCollection('recipes');
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/blog/${post.id}/`
+    items: recipes.map((recipe) => ({
+      ...recipe.data,
+      link: `/recipes/${recipe.id}/`
     }))
   });
 }
